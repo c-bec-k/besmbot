699f197c8bd25ed251c21ce946619e729e5765b5
Christopher Ruthenbeck <cruthenbeck@gmail.com>
2020-10-20 11:30:35 -0400

    fixed username/nickname error

--- commands/roll.js
+++ commands/roll.js
@@ -76,7 +76,7 @@ module.exports = {
       diceToRoll += edgeObs.length;
     }
     const diceResults = getDiceResults(diceToRoll);
-    const output = generateOutput(staticBonus, diceResults, edgeObs, message.author);
+    const output = generateOutput(staticBonus, diceResults, edgeObs, message.member.nickname);
     message.reply({embed: output});
 
   }
